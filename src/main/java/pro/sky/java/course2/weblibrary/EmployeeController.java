b44db9ce2373f2451b37a90964de555e43f4ad32
Karpukhin-EV <evkarpukhin@mail.ru>
2022-03-26 20:51:26 +0300

    Second Commit

---
+++ src/main/java/pro/sky/java/course2/weblibrary/EmployeeController.java
@@ -0,0 +1,47 @@
+package pro.sky.java.course2.weblibrary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/hallo")
+    public String welcome() {
+        return "Добро пожаловать в БД сотрудников!";
+    }
+
+    @GetMapping("/add")
+    public Employee add(@RequestParam(value = "firstName") String sf, @RequestParam(value = "lastName") String sl) {
+        Employee result;
+        result = employeeService.addEmployee(sf, sl);
+
+        return result;
+    }
+
+    @GetMapping("/remove")
+    public Employee remove(@RequestParam(value = "firstName") String sf, @RequestParam(value = "lastName") String sl) {
+        Employee result;
+        result = employeeService.removeEmployee(sf, sl);
+
+        return result;
+    }
+
+    @GetMapping("/find")
+    public Employee find(@RequestParam(value = "firstName") String sf, @RequestParam(value = "lastName") String sl) {
+        Employee result;
+        result = employeeService.findEmployee(sf, sl);
+
+        return result;
+    }
+
+
+}
---
+++ src/main/java/pro/sky/java/course2/weblibrary/fullArrayEmployeeException.java
@@ -0,0 +1,9 @@
+package pro.sky.java.course2.weblibrary;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class fullArrayEmployeeException extends RuntimeException {
+    // Возвращает статус 500 Internal Server Error.
+}
