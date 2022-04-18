c2a2b8c98c7a2da179f9a9787e61b36cfeef0352
Karpukhin-EV <evkarpukhin@mail.ru>
2022-04-18 23:45:45 +0300

    First Commit for hw10

--- src/main/java/pro/sky/java/course2/weblibrary/Sevices/Impl/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/java/course2/weblibrary/Sevices/Impl/EmployeeServiceImpl.java
@@ -1,5 +1,6 @@
 package pro.sky.java.course2.weblibrary.Sevices.Impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import pro.sky.java.course2.weblibrary.Data.Employee;
 import pro.sky.java.course2.weblibrary.Exceptions.BadEmployeeException;
@@ -12,35 +13,41 @@ import java.util.stream.Collectors;
 public class EmployeeServiceImpl implements EmployeeService {
     private final Map<String, Employee> employees = new HashMap<>(10, 0.75f);
 
-    private boolean isNotValue(String a, String b) {
-        return (a.isEmpty() || b.isEmpty());
+    private boolean checkValues(String a, String b) {
+        return !( StringUtils.isAlpha(a) && StringUtils.isAlpha(b) );
     }
 
     @Override
     public Employee addEmployee(String firstName, String lastName, int department, int salary) {
-        if (isNotValue(firstName, lastName)) {
+        if (checkValues(firstName, lastName)) {
             throw new BadEmployeeException();
         }
 
-        Employee employee = new Employee(firstName, lastName, department, salary);
-        if (employees.containsKey(firstName + lastName)) {
+        String firstName1 = StringUtils.capitalize(firstName);
+        String lastName1 = StringUtils.capitalize(lastName);
+
+        Employee employee = new Employee(firstName1, lastName1, department, salary);
+        if (employees.containsKey(firstName1 + lastName1)) {
             throw new BadEmployeeException();
         }
 
-        employees.put(firstName + lastName, employee);
+        employees.put(firstName1 + lastName1, employee);
         System.out.println("Сотрудник " + employee.getFirstName() + " " + employee.getLastName() + " " + " добавлен.");
         return employee;
     }
 
     @Override
     public Employee removeEmployee(String firstName, String lastName, int department, int salary) {
-        if (isNotValue(firstName, lastName)) {
+        if (checkValues(firstName, lastName)) {
             throw new BadEmployeeException();
         }
 
-        Employee employee = new Employee(firstName, lastName, department, salary);
-        if (employees.containsKey((firstName + lastName))) {
-            employees.remove(firstName + lastName);
+        String firstName1 = StringUtils.capitalize(firstName);
+        String lastName1 = StringUtils.capitalize(lastName);
+
+        Employee employee = new Employee(firstName1, lastName1, department, salary);
+        if (employees.containsKey((firstName1 + lastName1))) {
+            employees.remove(firstName1 + lastName1);
             System.out.println("Сотрудник " + employee.getFirstName() + " " + employee.getLastName() + " " + " удален.");
             return employee;
         }
@@ -49,12 +56,15 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee findEmployee(String firstName, String lastName, int department, int salary) {
-        if (isNotValue(firstName, lastName)) {
+        if (checkValues(firstName, lastName)) {
             throw new BadEmployeeException();
         }
 
-        Employee employee = new Employee(firstName, lastName, department, salary);
-        if (employees.containsKey(firstName + lastName)) {
+        String firstName1 = StringUtils.capitalize(firstName);
+        String lastName1 = StringUtils.capitalize(lastName);
+
+        Employee employee = new Employee(firstName1, lastName1, department, salary);
+        if (employees.containsKey(firstName1 + lastName1)) {
             System.out.println("Сотрудник " + employee.getFirstName() + " " + employee.getLastName() + " " + " найден.");
             return employee;
         }
